e69dec189780fc714d96182ba714427745da603f
maciej <maciejsz0@o2.pl>
2022-03-01 23:20:48 +0100

    Updates

---
+++ precache-manifest.72f5c84153ff20e825b47aa313e3cce3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb1b387d3970a3cffa8df674a96a334",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "6f0d9899141eb2f25788",
+    "url": "/restaurant/static/css/main.221ce3a9.chunk.css"
+  },
+  {
+    "revision": "4c49a47f79b1a6f6f6a9",
+    "url": "/restaurant/static/js/2.bb201596.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/restaurant/static/js/2.bb201596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0d9899141eb2f25788",
+    "url": "/restaurant/static/js/main.83589242.chunk.js"
+  },
+  {
+    "revision": "d8b34c76c7241bdb1bc6",
+    "url": "/restaurant/static/js/runtime-main.874cb75f.js"
+  },
+  {
+    "revision": "62fab0768f9ee51331dfc4dd1be86c53",
+    "url": "/restaurant/static/media/cake.62fab076.jpg"
+  },
+  {
+    "revision": "e03ba2966388d508ffead24fc7ba7ae1",
+    "url": "/restaurant/static/media/kitchen.e03ba296.jpg"
+  },
+  {
+    "revision": "23af7da1d342ad56c4ec6b133966dca5",
+    "url": "/restaurant/static/media/logo.23af7da1.png"
+  },
+  {
+    "revision": "0d9d6a4f5877cd65360ed3852b7bd8f0",
+    "url": "/restaurant/static/media/main1.0d9d6a4f.jpg"
+  },
+  {
+    "revision": "4717b54497728b24a64de3bae09dc9a5",
+    "url": "/restaurant/static/media/main2.4717b544.jpg"
+  },
+  {
+    "revision": "4bb68ff1b9dd864d774fd92061cca984",
+    "url": "/restaurant/static/media/main3.4bb68ff1.jpg"
+  },
+  {
+    "revision": "ddd655fd78dae510dd12aa5184eade02",
+    "url": "/restaurant/static/media/pancake.ddd655fd.jpg"
+  },
+  {
+    "revision": "3faa741ae64bd41e3d52b8bfcfd1c770",
+    "url": "/restaurant/static/media/patio.3faa741a.jpg"
+  },
+  {
+    "revision": "b93afe61737de7b731807dc05c4820c1",
+    "url": "/restaurant/static/media/pizza.b93afe61.jpg"
+  },
+  {
+    "revision": "135e98b14904a550e1eac485b7fc4732",
+    "url": "/restaurant/static/media/wedding.135e98b1.jpg"
+  }
+]);
